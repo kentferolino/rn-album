9a51f0ac78d69f893c5cf96837107a7e3615f7a3
Kent <kentfero@gmail.com>
2019-07-09 00:44:22 +0800

    Add Header component.

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import Header from './src/components/header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Test 1 Open up App.js to start working on your app!</Text>
-    </View>
+    <Header />
   );
 }
 
